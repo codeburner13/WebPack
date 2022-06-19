53ebbcbf5591f88e3281492f262353b0aa85b128
codeburner13 <kashiryaeva@gmail.com>
2022-06-19 19:21:03 +0300

    dz ko vtoromu uroku

---
+++ webpack-example/webpack.config.js
@@ -0,0 +1,48 @@
+
+const { resolve } = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const AnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+  entry: './js/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.[contenthash].js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\\.(png|jpe?g|gif|mp3)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'index.html')
+    }),
+    new AnalyzerPlugin()
+  ],
+  devServer: {
+    watchFiles: ["./*"],
+    port: 3000,
+    open: true,
+    hot: true
+  }
+} 
